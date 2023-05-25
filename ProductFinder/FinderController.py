7ac68f2b7c67aee368f642474d5d89d96837ba67
nanor <ttqfwfmg@gmail.com>
2023-05-25 13:30:48 -0700

    fix memory usage

--- ProductFinder/FinderController.py
+++ ProductFinder/FinderController.py
@@ -4,6 +4,7 @@ from FinderView import FinderView
 from memory_profiler import memory_usage
 import time
 
+
 # The FinderController class is responsible for controlling the flow of the program.
 class FinderController:
     def __init__(self, model: FinderModel, view: FinderView, settings: dict):
@@ -36,7 +37,7 @@ class FinderController:
         test_cases = [[108335], [108335, 391825, 340367, 286457, 661741],
                       [281610, 342706, 111873, 198029, 366109, 287261, 76283, 254489, 258540, 286457],
                       [427230, 372539, 396879, 391680, 208660, 105912, 332555, 227534, 68048, 188856, 736830, 736831,
-                      479020, 103313, 1],
+                       479020, 103313, 1],
                       [633, 1321, 3401, 5329, 10438, 372539, 396879, 16880, 208660, 105912, 332555, 227534, 68048,
                        188856, 736830, 736831, 479020, 103313, 1, 20373]]
         print("Test cases: ", test_cases)
@@ -51,14 +52,19 @@ class FinderController:
 
                     # Start the timer and memory usage tracker.
                     start_time = time.time()
-                    mem_usage_before = memory_usage(-1, interval=0.1, timeout=1)[0]
+                    #mem_usage_before = memory_usage(-1, interval=0.1, timeout=1)[0]
 
+                    def memory_test():
+                        return self.model.get_optimal_path(self.settings, destination_list)
+
+                    mem_usage = memory_usage(memory_test)
+                    max_mem_usage = max(mem_usage)
                     # Find the optimal path.
-                    optimal_path = self.model.get_optimal_path(self.settings, destination_list)
-                    mem_usage_after = memory_usage(-1, interval=0.1, timeout=1)[0]
+                    optimal_path = memory_test();
+                    #mem_usage_after = memory_usage((self.model.get_optimal_path,), -1, interval=0.1, timeout=1)[0]
                     end_time = time.time()
 
-                    mem_usage_diff = mem_usage_after - mem_usage_before
+                    #mem_usage_diff = mem_usage_after - mem_usage_before
                     time_diff = end_time - start_time
 
                     # Print the map with the optimal path highlighted.
@@ -72,7 +78,7 @@ class FinderController:
 
                     print("Destination list: ", destination_list)
                     print("Algorithm: ", self.settings['algorithm'])
-                    print(f"Memory usage: {mem_usage_diff} MiB")
+                    print(f"Memory usage: {max_mem_usage} MiB")
                     print(f"Running time: {time_diff} seconds")
 
                     if input('Continue? y/n?') == 'n':
@@ -98,7 +104,8 @@ class FinderController:
                                      mapSize=self.settings['mapSize'])
 
             # Ask the user if they want to get another product.
-            option = input("Choose one option: \n1) go get other products! \n2) export results to a file \n3) back to main menu\n")
+            option = input(
+                "Choose one option: \n1) go get other products! \n2) export results to a file \n3) back to main menu\n")
             if option == "1":
                 continue
             elif option == "2":
